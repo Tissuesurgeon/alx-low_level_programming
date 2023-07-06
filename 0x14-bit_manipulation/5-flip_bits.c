a387180299fb738b4a80fcb5987f815a6932e817
Richard Frimpong Karikari <pearlgabby0@gmail.com>
2023-07-06 15:42:26 -0700

    added the flip_bits file

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - a function that returns the number of bits to flip to get from one number to 
+ * another
+ * @n: a
+ * @m: a
+ * Return: Always 0.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int sol = n ^ m;
+	unsigned long int cant = 0;
+
+	while (sol)
+	{
+		cant += sol & 1;
+		sol = sol >> 1;
+	}
+	return (cant);
+}
