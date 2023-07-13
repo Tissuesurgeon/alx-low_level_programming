9c887222961647c3760c2a99ccb0a07b06c625de
Richard Frimpong Karikari <pearlgabby0@gmail.com>
2023-07-13 02:05:13 -0700

    a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
 /**
- * read_textfile - function that converts a binary number to an unsigned int
+ * read_textfile -a function that converts a binary number to an unsigned int
  * @filename: a
  * @letters: a
  * Return: Always 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int x;
+	int fd;
 	char *buffer;
-	ssize_t numBytes;
+	ssize_t num_bytes;
 	ssize_t i;
 
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY, 0777);
-	if (x == -1)
+	if (fd == -1)
 		return (0);
 	buffer = malloc(letters * sizeof(char));
 	if (!buffer)
 		return (0);
-	numBytes = read(x, buffer, letters);
-	if (numBytes == -1)
+	num_bytes = read(fd, buffer, letters);
+	if (num_bytes == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	for (i = 0; i < numBytes; i++)
+	for (i = 0; i < num_bytes; i++)
 	{
 		write(STDOUT_FILENO, &buffer[i], 1);
 	}
-	if (close(x) == -1)
+	if (close(fd) == -1)
 		return (0);
 	free(buffer);
-	return (numBytes);
+	return (num_bytes);
 }
